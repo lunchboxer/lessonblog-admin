ebfafd726fd9494aa7b52a67dd3047cbed405948
James <metal.lunchbox@gmail.com>
2018-12-24 12:41:51 +0800

    remove i18n

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,5 @@
 import { Store } from 'svelte/store.js'
 import { APPNAME } from './constants.js'
-import { i18n } from 'svelte-i18n'
-import { enUs } from '../locales/en-US.js'
-import { zhCn } from '../locales/zh-CN.js'
 
 let store = new Store({
   APPNAME,
@@ -15,13 +12,5 @@ let store = new Store({
   showAddGroupForm: false,
   showCreateNewUserForm: false
 })
-
-store = i18n(store, {
-  dictionary: {
-    'en-US': enUs,
-    'zh-CN': zhCn
-  }
-})
-store.i18n.setLocale('zh-CN')
 
 export default store
