f4542d71c57d9dc5b1c31882067e50213a05259d
James <metal.lunchbox@gmail.com>
2019-01-01 23:23:55 +0800

    add feathers reactive

--- src/utils/feathers.js
+++ src/utils/feathers.js
@@ -4,11 +4,13 @@ import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
 import { CookieStorage } from 'cookie-storage'
 import { APIURL } from './constants'
+import rx from 'feathers-reactive'
 
 const socket = io(APIURL)
 const client = feathers()
 
 client.configure(socketio(socket, { timeout: 10000 }))
+client.configure(rx({ idField: '_id' }))
 client.configure(auth({ storage: new CookieStorage() }))
 
 export default client
